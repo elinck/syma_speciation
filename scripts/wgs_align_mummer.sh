e160e54be1d548ac189cb158c83a019d3b825cc6
Ethan Linck <ethanblinck@gmail.com>
2019-07-19 12:24:29 -0600

    add mummer loop

---
+++ scripts/wgs_align_mummer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /media/burke/bigMac/ethan/
+
+refSeqs=tgut2_split/* # path to zebra finch reference genome folder
+
+for chr in $refSeqs
+
+    do
+    echo "aligning to $chr"
+
+    nucmer $chr /media/burke/bigMac/ethan/masked.ref.fa --maxgap 1000 # boost from default of 90 to account for structural rearrangements in divergent ref
+
+    show-coords ./out.delta > /media/burke/bigMac/ethan/mummer_out/coords/${chr##*/}.coords #Check $
+
+    mv out.delta /media/burke/bigMac/ethan/mummer_out/alignments/${chr##*/}.delta
+
+done
